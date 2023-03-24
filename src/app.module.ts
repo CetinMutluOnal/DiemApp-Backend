230214de34b6e33a90f65f8b8dde9bb1215b5cef
CetinMutluOnal <cetinmutluonal@gmail.com>
2023-03-24 18:07:55 +0300

    Created user service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './schema/user.schema';
+import { UserService } from './user/user.service';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27019', {
@@ -11,6 +12,6 @@ import { UserSchema } from './schema/user.schema';
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
---
+++ src/user/user.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from 'src/dto/user-dto/create-user.dto';
+import { UpdateUserDto } from 'src/dto/user-dto/update-user.dto';
+import { IUser } from 'src/interface/user.interface';
+import { NotFoundException } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+  constructor(@InjectModel('User') private userModel: Model<IUser>) {}
+
+  async createUser(createUserdto: CreateUserDto): Promise<IUser> {
+    const newUser = await new this.userModel(createUserdto);
+    return newUser.save();
+  }
+
+  async updateUser(
+    userId: string,
+    updateUserDto: UpdateUserDto,
+  ): Promise<IUser> {
+    const existingUser = await this.userModel.findByIdAndUpdate(
+      userId,
+      updateUserDto,
+      { new: true },
+    );
+    if (!existingUser) {
+      throw new NotFoundException(`User #${userId} not found`);
+    }
+    return existingUser;
+  }
+
+  async getAllUser(): Promise<IUser[]> {
+    const users = await this.userModel.find();
+    if (!users || users.length == 0) {
+      throw new NotFoundException('Users data not found');
+    }
+    return users;
+  }
+
+  async getUser(userId: string): Promise<IUser> {
+    const user = await this.userModel.findById(userId).exec();
+    if (!user) {
+      throw new NotFoundException(`User #${userId} not found`);
+    }
+    return user;
+  }
+
+  async deleteUser(userId: string): Promise<IUser> {
+    const deletedUser = await this.userModel.findByIdAndDelete(userId);
+    if (!deletedUser) {
+      throw new NotFoundException(`User #${userId} not found`);
+    }
+    return deletedUser;
+  }
+}
