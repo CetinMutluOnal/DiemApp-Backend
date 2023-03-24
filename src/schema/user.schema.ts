15505a578aac42284293b8d18cb321ae6a9623ec
CetinMutluOnal <cetinmutluonal@gmail.com>
2023-03-24 16:44:14 +0300

    Created&Imported user schema

---
+++ src/schema/user.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class User {
+  @Prop()
+  name: string;
+
+  @Prop()
+  username: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+
+  @Prop()
+  avatar: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
