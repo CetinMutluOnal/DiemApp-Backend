07236511a87fb91e85b4d7946fdb505b93103e62
Çetin Mutlu <94856045+CetinMutluOnal@users.noreply.github.com>
2023-03-25 15:48:34 +0300

    Revert "Feature/jwt authorization"

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,23 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserSchema } from './schema/user.schema';
-import { UserService } from './user/user.service';
-import { UserController } from './user/user.controller';
-import { NestjsFormDataModule } from 'nestjs-form-data';
-import { ConfigModule } from '@nestjs/config/dist';
-import { JwtModule } from '@nestjs/jwt/dist/jwt.module';
 @Module({
   imports: [
-    NestjsFormDataModule,
-    JwtModule.register({}),
-    ConfigModule.forRoot({}),
     MongooseModule.forRoot('mongodb://localhost:27019', {
       dbName: 'diemSocial',
     }),
-    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
   ],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/interface/user.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-
-export interface IUser extends Document {
-  readonly name: string;
-  readonly userName: string;
-  readonly email: string;
-  password: string;
-  avatar: string | undefined;
-}
