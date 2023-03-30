fd002c99f76bc70458736b7dd1a3a8dff1eab059
CetinMutluOnal <cetinmutluonal@gmail.com>
2023-03-30 09:38:56 +0300

    Configured Dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,27 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserSchema } from './schema/user.schema';
-import { NestjsFormDataModule } from 'nestjs-form-data';
-import { PostService } from './post/post.service';
-import { PostController } from './post/post.controller';
-import { PostSchema } from './schema/post.schema';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 @Module({
   imports: [
-    NestjsFormDataModule,
     MongooseModule.forRoot('mongodb://localhost:27019', {
       dbName: 'diemSocial',
     }),
-    MongooseModule.forFeature([
-      { name: 'User', schema: UserSchema },
-      { name: 'Post', schema: PostSchema },
-    ]),
     AuthModule,
     UserModule,
+    PostModule,
   ],
-  controllers: [AppController, PostController],
-  providers: [AppService, PostService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
