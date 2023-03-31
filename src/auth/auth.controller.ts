f3531e6231c46d4e6766768b57557c66ab40797c
CetinMutluOnal <cetinmutluonal@gmail.com>
2023-04-01 02:41:17 +0300

    Implemented auth guard for auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,23 +1,43 @@
-import { Controller, Post, Res, Body, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Res,
+  Body,
+  HttpStatus,
+  Request,
+  UseGuards,
+  HttpCode,
+} from '@nestjs/common';
 import { AuthUserDto } from 'src/dto/auth-dto/auth-user.dto';
 import { FormDataRequest } from 'nestjs-form-data';
+import { Public } from './auth.guard';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt.auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
-  @Post('/login')
+  @Public()
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
   @FormDataRequest()
   async signIn(@Res() response, @Body() authUserDto: AuthUserDto) {
     try {
       const signedUser = await this.authService.signIn(authUserDto);
-      return response.status(HttpStatus.CREATED).json({
-        token: signedUser.access_token,
+      return response.status(HttpStatus.OK).json({
+        access_token: signedUser.access_token,
       });
     } catch (error) {
       response.status(HttpStatus.FORBIDDEN).json({
         message: error.message,
       });
     }
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,18 +5,29 @@ import { AuthService } from './auth.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from 'src/schema/user.schema';
 import { JwtModule } from '@nestjs/jwt/dist';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { NestjsFormDataModule } from 'nestjs-form-data';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
     UserModule,
+    PassportModule,
     NestjsFormDataModule,
-    JwtModule.register({}),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        secret: config.get('JWT_SECRET'),
+        signOptions: { expiresIn: '60s' },
+      }),
+      inject: [ConfigService],
+    }),
     ConfigModule.forRoot({}),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  /* { provide: APP_GUARD, useClass: AuthGuard }],*/
 })
 export class AuthModule {}
