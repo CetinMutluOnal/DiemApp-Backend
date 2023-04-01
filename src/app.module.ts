8fc827b10921090c3f8b4a7da895a33c4d28d4c4
CetinMutluOnal <cetinmutluonal@gmail.com>
2023-04-02 00:36:01 +0300

    Imported follow module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { FollowModule } from './follow/follow.module';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27019', {
@@ -13,6 +14,7 @@ import { PostModule } from './post/post.module';
     AuthModule,
     UserModule,
     PostModule,
+    FollowModule,
   ],
   controllers: [AppController],
   providers: [AppService],
