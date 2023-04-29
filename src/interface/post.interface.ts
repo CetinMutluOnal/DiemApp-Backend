af09758f8fa9469532a2558926736ea2d0ee1daf
CetinMutluOnal <cetinmutluonal@gmail.com>
2023-04-29 23:54:33 +0300

    Implemented soft delete and refactored dto property of post.

--- src/interface/post.interface.ts
+++ src/interface/post.interface.ts
@@ -4,6 +4,6 @@ export interface IPost extends Document {
   userId: Types.ObjectId;
   content: string;
   readonly media: string;
-  readonly created_at: Date;
-  deleted_at: Date;
+  readonly createdAt: Date;
+  deletedAt: Date;
 }
