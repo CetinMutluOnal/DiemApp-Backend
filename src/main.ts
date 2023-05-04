763cd1dcf162273b5b23d18b9d5b09372706e629
CetinMutluOnal <cetinmutluonal@gmail.com>
2023-05-04 13:31:07 +0300

    Enabled CORS for frontend

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
   app.enableCors();
+  await app.listen(3000);
   app.useGlobalPipes(new ValidationPipe());
 }
 bootstrap();
