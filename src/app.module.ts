c1c600b4fdcbdf2aaa38bdee4d82fd83a8e8b142
CetinMutluOnal <cetinmutluonal@gmail.com>
2023-05-11 00:29:20 +0300

    Served static files

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,15 @@ import { FollowModule } from './follow/follow.module';
 import { MessageModule } from './message/message.module';
 import { LikeModule } from './like/like.module';
 import { CommentModule } from './comment/comment.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27019', {
       dbName: 'diemSocial',
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..'),
     }),
     AuthModule,
     UserModule,
